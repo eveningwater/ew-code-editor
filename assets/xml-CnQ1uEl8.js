f4d8254c69937ccf4ed9c42500543ae31ce4b2db
eveningwater.123 <eveningwater.123@bytedance.com>
2025-06-21 10:24:23 +0800

    Updates

--- assets/xml-CnQ1uEl8.js
+++ assets/xml-CnQ1uEl8.js
@@ -1,4 +1,4 @@
-import{m as r}from"./monaco-editor-aRhec9rV.js";/*!-----------------------------------------------------------------------------
+import{m as r}from"./monaco-editor-B-77GMOE.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
